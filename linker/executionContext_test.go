3916b5b31d842c55d1955527d47664fad6bb9a1f
jcollins-axway <jcollins@axway.com>
2019-08-08 15:05:49 -0700

    INT - fix test for previously updated methods

--- linker/executionContext_test.go
+++ linker/executionContext_test.go
@@ -64,52 +64,52 @@ func TestExecutionContext(t *testing.T) {
 		t.Error("incorrect Context received from GetSpanContext")
 	}
 
-	// Test GetStringConfig
-	if testExecutionContext.GetStringConfig("string-param") != configMap["string-param"].Value {
-		t.Errorf("The GetStringConfig method returned %s but we expected %s", testExecutionContext.GetStringConfig("string-param"), configMap["string-param"])
+	// Test GetStringConfigValue
+	if testExecutionContext.GetStringConfigValue("string-param") != configMap["string-param"].Value {
+		t.Errorf("The GetStringConfigValue method returned %s but we expected %s", testExecutionContext.GetStringConfigValue("string-param"), configMap["string-param"])
 	}
 
-	if testExecutionContext.GetStringConfig("int-param") != "" {
-		t.Error("The GetStringConfig method returned a value for an int type but we expected \"\"")
+	if testExecutionContext.GetStringConfigValue("int-param") != "" {
+		t.Error("The GetStringConfigValue method returned a value for an int type but we expected \"\"")
 	}
 
-	if testExecutionContext.GetStringConfig("param-does-not-exist") != "" {
-		t.Error("The GetStringConfig method returned a value for a non existent params but we expected \"\"")
+	if testExecutionContext.GetStringConfigValue("param-does-not-exist") != "" {
+		t.Error("The GetStringConfigValue method returned a value for a non existent params but we expected \"\"")
 	}
 
-	// Test GetIntConfig
+	// Test GetIntConfigValue
 	intVal, _ := strconv.Atoi(configMap["int-param"].Value)
-	if testExecutionContext.GetIntConfig("int-param") != intVal {
-		t.Errorf("The GetIntConfig method returned %d but we expected %s", testExecutionContext.GetIntConfig("int-param"), configMap["int-param"])
+	if testExecutionContext.GetIntConfigValue("int-param") != intVal {
+		t.Errorf("The GetIntConfigValue method returned %d but we expected %s", testExecutionContext.GetIntConfigValue("int-param"), configMap["int-param"])
 	}
 
-	if testExecutionContext.GetIntConfig("string-param") != 0 {
-		t.Error("The GetIntConfig method returned a value for a string type but we expected 0")
+	if testExecutionContext.GetIntConfigValue("string-param") != 0 {
+		t.Error("The GetIntConfigValue method returned a value for a string type but we expected 0")
 	}
 
-	if testExecutionContext.GetIntConfig("param-does-not-exist") != 0 {
-		t.Error("The GetIntConfig method returned a value for a non existent params but we expected 0")
+	if testExecutionContext.GetIntConfigValue("param-does-not-exist") != 0 {
+		t.Error("The GetIntConfigValue method returned a value for a non existent params but we expected 0")
 	}
 
-	if testExecutionContext.GetIntConfig("int-param-misconfig") != 0 {
-		t.Error("The GetIntConfig method returned a value for a non parsable value but we expected 0")
+	if testExecutionContext.GetIntConfigValue("int-param-misconfig") != 0 {
+		t.Error("The GetIntConfigValue method returned a value for a non parsable value but we expected 0")
 	}
 
-	// Test GetBooleanConfig
+	// Test GetBooleanConfigValue
 	boolVal, _ := strconv.ParseBool(configMap["boolean-param"].Value)
-	if testExecutionContext.GetBooleanConfig("boolean-param") != boolVal {
-		t.Errorf("The GetBooleanConfig method returned %t but we expected %s", testExecutionContext.GetBooleanConfig("boolean-param"), configMap["boolean-param"])
+	if testExecutionContext.GetBooleanConfigValue("boolean-param") != boolVal {
+		t.Errorf("The GetBooleanConfigValue method returned %t but we expected %s", testExecutionContext.GetBooleanConfigValue("boolean-param"), configMap["boolean-param"])
 	}
 
-	if testExecutionContext.GetBooleanConfig("string-param") != false {
-		t.Error("The GetBooleanConfig method returned a value for a string type but we expected false")
+	if testExecutionContext.GetBooleanConfigValue("string-param") != false {
+		t.Error("The GetBooleanConfigValue method returned a value for a string type but we expected false")
 	}
 
-	if testExecutionContext.GetBooleanConfig("param-does-not-exist") != false {
-		t.Error("The GetBooleanConfig method returned a value for a non existent params but we expected false")
+	if testExecutionContext.GetBooleanConfigValue("param-does-not-exist") != false {
+		t.Error("The GetBooleanConfigValue method returned a value for a non existent params but we expected false")
 	}
 
-	if testExecutionContext.GetBooleanConfig("boolean-param-misconfig") != false {
-		t.Error("The GetBooleanConfig method returned a value for a non parsable value but we expected false")
+	if testExecutionContext.GetBooleanConfigValue("boolean-param-misconfig") != false {
+		t.Error("The GetBooleanConfigValue method returned a value for a non parsable value but we expected false")
 	}
 }
